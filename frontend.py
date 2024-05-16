59654296f9a28eb0839bbf35ebc00065f46b8f6b
Ethan Weinhaus <eweinhaus@elon.edu>
2024-05-16 14:08:25 -0400

    initial commit

--- frontend.py
+++ frontend.py
@@ -2,7 +2,6 @@ import dash
 from dash import dcc
 from dash import html
 import pandas as pd
-import ipdb
 import constants
 
 num_sims = str(constants.NUM_SIMULATIONS)
@@ -10,111 +9,107 @@ num_sims = str(constants.NUM_SIMULATIONS)
 def create_layout():
     return html.Div([
         html.Div(id="go", style={'display': 'none'}),
-        dcc.Store(id = "game_results_JSON"),
-        dcc.Store(id = "future_games_JSON"),
-        dcc.Store(id = "standings_JSON"),
-        dcc.Store(id = "adj_standings_JSON"),
-        dcc.Store(id = "power_rank_JSON"),
+        dcc.Store(id="game_results_JSON"),
+        dcc.Store(id="future_games_JSON"),
+        dcc.Store(id="standings_JSON"),
+        dcc.Store(id="adj_standings_JSON"),
+        dcc.Store(id="power_rank_JSON"),
 
-        html.Div (
-            className = "table_holder_top",
-            children = [
-                html.H1("Boston MABL Playoff Predictor")
-            ]
-        ),
-
-        html.Div(
-            className = "table_holder_1",
-            children = [
-                html.H2("Standings"),
-                html.Table(id="standings_table")
-            ]
-        ),
-        html.Div(
-            className = "table_holder_2",
-            children = [
-                html.H2("Power Rankings"),
-                dcc.Loading(
-                    id="loading-1",
-                    type="default",  # or 'graph', 'cube', 'circle', 'dot', or 'default'
-                    fullscreen=False,  # Set to True to make the loading component take up the whole screen
-                    children=[
-                        html.Div(id="power_rank_graph_loader")
-                    ],
-                    className="custom-loading",
-                    style={'position': 'absolute', 'left': '50%', 'top': '100px'},  # Center the loading component
-                ),
-            ]
-        ),
-        html.Div(
-            className = "table_holder_3",
-            children = [
-                html.H2("Single Game Win Probability"),
-                html.Div(
-                    id = "dropdown_title_left",
-                    className = "dropdown_titles", 
-                    children = [
-                        html.H3("Away Team"),
-                    ]
-                ),
-                html.Div(
-                    id = "dropdown_title_right", 
-                    className = "dropdown_titles",
-                    children = [
-                        html.H3("Home Team"),
-                    ]
-                ),
-                html.Div(
-                    id = "dropdown_holder",
-                    children = [
-                        dcc.Dropdown(
-                            id='away_team_dropdown',
-                            ),
-                        dcc.Dropdown(
-                            id='home_team_dropdown',
-                        ),
-                    ]
-                ),
-                html.Div(
-                    id = "win_prob_left",
-                    className = "win_prob_holders",
-                    children = [
-                        html.H2(id='away_win_prob'),
-                    ]
-                ),
-                html.Div(
-                    id = "win_prob_right",
-                    className = "win_prob_holders",
-                    children = [
-                        html.H2(id='home_win_prob'),
-                    ]
-                ),
-            ]
-        ),
-        html.Div(
-            className="table_holder_4",
+        html.Nav(
+            className="navbar",
             children=[
-                html.H2("Playoff Probability"),
-                dcc.Loading(
-                    id="loading-2",
-                    type="default",  # or 'graph', 'cube', 'circle', 'dot', or 'default'
-                    fullscreen=False,  # Set to True to make the loading component take up the whole screen
-                    children=[
-                        html.Div(
-                            id="playoff_graph_loader"
-                            # Children will be dynamically updated content
-                        )
-                    ],
-                    className="custom-loading",
-                    style={'position': 'absolute', 'left': '50%', 'top': '75px'},  # Center the loading component
-                ),
-                html.Div(
-                    id = "loading_message",
-                    style={'margin-top': '175px', 'margin-bottom': '25px'},  # Center the loading component
-                    children=[
-                        html.H3("Simulating remainder of season "+ num_sims +" times. This may take a minute..."),
-                    ]
-                ),
+                html.H1("Boston MABL Playoff Predictor"),
+                html.Ul([
+                    html.Li(html.A("Overview", href="#overview")),
+                    html.Li(html.A("Standings", href="#standings")),
+                    html.Li(html.A("Power Rankings", href="#power-rankings")),
+                    html.Li(html.A("Single Game Win Probability", href="#win-probability")),
+                    html.Li(html.A("Playoff Probability", href="#playoff-probability")),
+                ])
             ]
         ),
+
+        html.Div(id="overview", className="section", children=[
+            html.H2("Overview"),
+            html.Div(className="content", children=[
+                html.P("This Dash app is a live updating tool that predicts the playoff chances of each team in the Boston MABL. The app crawls the league website to get the results of all games played so far this season. It then uses these results to calculate the current standings and power rankings of each team. From there it creates a Monte Carlo simulation to simulate the remainder of the season a specified number of times. The app then calculates the playoff chances of each team based on the results of the simulations.", 
+                       className="left", 
+                ),
+            ]),
+            html.Div(
+                [html.Img(src="/assets/BMBL_Logo.png", className="logo")], 
+                className="right"),
+        ]),
+
+        html.Div(id="standings", className="section", children=[
+            html.H2("Standings"),
+            html.Div(className="content", children=[
+                html.Div([
+                    html.Table(id="standings_table"),
+                ], className="left"),
+                html.P("This page uses a webcrawler to grab outcome of each game from the league website and create live league standings, updated to the minute.", className="right")
+            ])
+        ]),
+
+        html.Div(id="power-rankings", className="section", children=[
+            html.H2("Power Rankings"),
+            html.Div(className="content", children=[
+                html.Div([
+                    dcc.Loading(
+                        id="loading-1",
+                        type="default",
+                        children=html.Div(id="power_rank_graph_loader"),
+                        className="custom-loading"
+                    ),
+                ], className="left"),
+                html.P("The power rankungs page takes each team's performance thus far (Runs Scored, Runs Allowed, and Games Played) and applies Bill James' Pythagorean Win Probability formula to estimate each team's expected win percentage vs. a league average team. It then uses a derivative of Tangotigers win probability forumla, factors in each team's home field advantage and strength of schedule into pythagorean win percentage in order to get adjusted expected winning percentage, or 'Power Rank'", className="right")
+            ])
+        ]),
+
+        html.Div(id="win-probability", className="section", children=[
+            html.H2("Single Game Win Probability"),
+            html.Div(className="content", children=[
+                html.Div([
+                    html.Div(className="dropdown-holder", children=[
+                        html.Div(className="dropdown", children=[
+                            html.H3("Away Team"),
+                            dcc.Dropdown(id='away_team_dropdown'),
+                        ]),
+                        html.Div(className="dropdown", children=[
+                            html.H3("Home Team"),
+                            dcc.Dropdown(id='home_team_dropdown'),
+                        ]),
+                    ]),
+                    html.Div(className="win-prob-holder", children=[
+                        html.Div(id='away_win_prob', className="win-prob"),
+                        html.Div(id='home_win_prob', className="win-prob"),
+                    ]),
+                ], className="left"),
+                html.P("This page applies Tangotiger's win probability formula to each team's power rank in order to estimate the percentage chance any given home team beats any given away team in a potential matchup. It utilizes Dash callbacks in order to allow user to input any potential matchup and estimate outcome in real time.", className="right")
+            ])
+        ]),
+
+        html.Div(id="playoff-probability", className="section", children=[
+            html.H2("Playoff Probability"),
+            html.Div(className="content", children=[
+                html.Div([
+                    dcc.Loading(
+                        id="loading-2",
+                        type="default",
+                        children=html.Div(id="playoff_graph_loader"),
+                        className="custom-loading"
+                    ),
+                    html.Div(id="loading_message", children=[
+                        html.H3(f"Simulating remainder of season {num_sims} times. This may take a minute..."),
+                    ]),
+                ], className="left"),
+                html.P("Given current standings, power rankings, individual matchup win probability, and remaining schedule, app uses Pandas and Numpy to run a Monte Carlo simulation to simulate the rest of the season 1000 times and graphs the probability that each team finishes top 6 in the standings in order to make the league playoffs.", className="right")
+            ])
+        ]),
     ])
+
+app = dash.Dash(__name__)
+app.layout = create_layout()
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
