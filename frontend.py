4abdc08467e74ef4fcb5b72c24c61c59b069846f
Ethan Weinhaus <eweinhaus@elon.edu>
2024-05-16 16:48:33 -0400

    updated changes, incomplete

--- frontend.py
+++ frontend.py
@@ -18,7 +18,7 @@ def create_layout():
         html.Nav(
             className="navbar",
             children=[
-                html.H1("Boston MABL Playoff Predictor"),
+                html.H1("BMBL Playoff Predictor"),
                 html.Ul([
                     html.Li(html.A("Overview", href="#overview")),
                     html.Li(html.A("Standings", href="#standings")),
@@ -32,13 +32,19 @@ def create_layout():
         html.Div(id="overview", className="section", children=[
             html.H2("Overview"),
             html.Div(className="content", children=[
-                html.P("This Dash app is a live updating tool that predicts the playoff chances of each team in the Boston MABL. The app crawls the league website to get the results of all games played so far this season. It then uses these results to calculate the current standings and power rankings of each team. From there it creates a Monte Carlo simulation to simulate the remainder of the season a specified number of times. The app then calculates the playoff chances of each team based on the results of the simulations.", 
-                       className="left", 
-                ),
-            ]),
-            html.Div(
-                [html.Img(src="/assets/BMBL_Logo.png", className="logo")], 
-                className="right"),
+                html.Img(src="/assets/BMBL_Logo.png", className="left"),
+                html.Div(className="right", children=[
+                    html.P([
+                        "This Dash app is a live updating tool that predicts the playoff chances of each team in the Boston Metro Baseball League. ",
+                        "The app crawls the league website to get the results of all games played so far this season. ",
+                        "It then uses these results to calculate the current standings and power rankings of each team. ",
+                        "From there it creates a Monte Carlo simulation to simulate the remainder of the season a specified number of times. ",
+                        "The app then calculates the playoff chances of each team based on the results of the simulations. ",
+                        html.A("See more here", href="https://github.com/eweinhaus/baseball-power-rankings/blob/main/README.md", target="_blank"),
+                        "."
+                    ])
+                ])
+            ])
         ]),
 
         html.Div(id="standings", className="section", children=[
@@ -47,7 +53,9 @@ def create_layout():
                 html.Div([
                     html.Table(id="standings_table"),
                 ], className="left"),
-                html.P("This page uses a webcrawler to grab outcome of each game from the league website and create live league standings, updated to the minute.", className="right")
+                html.Div(className="right", children=[
+                    html.P("This page uses a webcrawler to grab outcome of each game from the league website and create live league standings, updated to the minute.")
+                ])
             ])
         ]),
 
@@ -62,7 +70,9 @@ def create_layout():
                         className="custom-loading"
                     ),
                 ], className="left"),
-                html.P("The power rankungs page takes each team's performance thus far (Runs Scored, Runs Allowed, and Games Played) and applies Bill James' Pythagorean Win Probability formula to estimate each team's expected win percentage vs. a league average team. It then uses a derivative of Tangotigers win probability forumla, factors in each team's home field advantage and strength of schedule into pythagorean win percentage in order to get adjusted expected winning percentage, or 'Power Rank'", className="right")
+                html.Div(className="right", children=[
+                    html.P("The power rankings page takes each team's performance thus far (Runs Scored, Runs Allowed, and Games Played) and applies Bill James' Pythagorean Win Probability formula to estimate each team's expected win percentage vs. a league average team. It then uses a derivative of Tangotigers win probability forumla, factors in each team's home field advantage and strength of schedule into pythagorean win percentage in order to get adjusted expected winning percentage, or 'Power Rank'.")
+                ])
             ])
         ]),
 
@@ -85,7 +95,9 @@ def create_layout():
                         html.Div(id='home_win_prob', className="win-prob"),
                     ]),
                 ], className="left"),
-                html.P("This page applies Tangotiger's win probability formula to each team's power rank in order to estimate the percentage chance any given home team beats any given away team in a potential matchup. It utilizes Dash callbacks in order to allow user to input any potential matchup and estimate outcome in real time.", className="right")
+                html.Div(className="right", children=[
+                    html.P("This page applies Tangotiger's win probability formula to each team's power rank in order to estimate the percentage chance any given home team beats any given away team in a potential matchup. It utilizes Dash callbacks in order to allow user to input any potential matchup and estimate outcome in real time.")
+                ])
             ])
         ]),
 
@@ -103,7 +115,9 @@ def create_layout():
                         html.H3(f"Simulating remainder of season {num_sims} times. This may take a minute..."),
                     ]),
                 ], className="left"),
-                html.P("Given current standings, power rankings, individual matchup win probability, and remaining schedule, app uses Pandas and Numpy to run a Monte Carlo simulation to simulate the rest of the season 1000 times and graphs the probability that each team finishes top 6 in the standings in order to make the league playoffs.", className="right")
+                html.Div(className="right", children=[
+                    html.P("Given current standings, power rankings, individual matchup win probability, and remaining schedule, app uses Pandas and Numpy to run a Monte Carlo simulation to simulate the rest of the season 1000 times and graphs the probability that each team finishes top 6 in the standings in order to make the league playoffs.")
+                ])
             ])
         ]),
     ])
